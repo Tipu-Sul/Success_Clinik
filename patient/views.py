493050793d276bda1c00f6c0772085b0c23346a6
Tipu Sultan <sultanmdtipu1996@gmail.com>
2024-02-09 11:44:18 +0800

    changes-4

--- patient/views.py
+++ patient/views.py
@@ -27,7 +27,8 @@ class UserRegisterView(APIView):
             user=serial.save()
             token=default_token_generator.make_token(user)
             uid=urlsafe_base64_encode(force_bytes(user.pk))
-            confirm_link=f'http://127.0.0.1:8000/patient/active/{uid}/{token}'
+            # confirm_link=f'http://127.0.0.1:8000/patient/active/{uid}/{token}'
+            confirm_link=f'https://success-clinic.onrender.com/patient/active/{uid}/{token}'
             email_subject="Conformation Link"
             email_body=render_to_string('confirm_link.html',{'confirm_link':confirm_link})
             email=EmailMultiAlternatives(email_subject,"",to=[user.email])
